b51928ab2f429acdc282c9c74d95a34ea1b861ba
Sander Onland <onland@alias.nl>
2012-06-29 12:09:10 +0200

    added dutch translation

---
+++ lib/numbers_and_words/translations_helpers/nl.rb
@@ -0,0 +1,42 @@
+module NumbersAndWords
+  module TranslationsHelpers
+    module Nl
+      include NumbersAndWords::TranslationsHelpers::Base
+
+      private
+
+      def translation_teens number
+        t(:teens)[number]
+      end
+
+      def translation_tens number
+        t(:tens)[number]
+      end
+
+      def translation_ones number
+        t(:ones)[number]
+      end
+
+      def translation_tens_with_ones numbers
+        [ translation_ones(numbers[0]), translation_tens(numbers[1])].join 'en'
+      end
+
+      def translation_union
+        t :union
+      end
+
+      def translation_hundreds number
+        return t(:hundreds) if number==1
+        [t(:ones)[number], t(:hundreds)].join ''
+      end
+
+      def translation_megs capacity
+        t(:mega)[capacity]
+      end
+      
+      def zero
+        t(:ones)[0]
+      end
+    end
+  end
+end
